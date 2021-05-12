2c9d15ff669b58a5381b378cac1b51abfc70ee9e
Ryan Cabeen <cabeen@gmail.com>
2021-05-11 18:06:58 -0700

    updates

--- docs/reports/2021-05-11/classify/run.sh
+++ docs/reports/2021-05-11/classify/run.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+#
+# This is an example of how to run the script.  This assumes you have all of the
+# necessary R packages installed already.  It also assumes you have the output
+# table directory stored in `../tables`.
 
 cd $(dirname $0)
-Rscript run.R
+Rscript run.R ../tables
+
