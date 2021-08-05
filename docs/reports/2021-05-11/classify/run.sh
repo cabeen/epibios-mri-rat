6667d6ff88fae7f1c41f4f93b66baac6ac3d5838
Ryan Cabeen <rcabeen@RyBookPro.local>
2021-08-05 09:35:50 -0700

    updates

--- docs/reports/2021-05-11/classify/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-# This is an example of how to run the script.  This assumes you have all of the
-# necessary R packages installed already.  It also assumes you have the output
-# table directory stored in `../tables`.
-
-cd $(dirname $0)
-Rscript run.R ../tables
-
